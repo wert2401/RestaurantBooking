7472624d7355a70a34fb3e53e2f2e360fe11bf9e
Ilya Fedorov <ilya.f.s01@mail.ru>
2023-06-19 00:04:02 +0700

    Fix: multiple reviews per user

---
+++ RestaurantBooking.Data/Migrations/20230618170023_MultipleRatesPerUser.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantBooking.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MultipleRatesPerUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Review_UserId_RestaurantId",
+                table: "Review");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Review_UserId",
+                table: "Review",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Review_UserId",
+                table: "Review");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Review_UserId_RestaurantId",
+                table: "Review",
+                columns: new[] { "UserId", "RestaurantId" },
+                unique: true);
+        }
+    }
+}
